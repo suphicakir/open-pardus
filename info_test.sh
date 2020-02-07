b442a6d28095bb01a3da29625a5ff0372e88e618
suphicakir <suphicakir@gmail.com>
2020-02-07 23:19:20 +0300

    info_test.sh dosyası silindi.

--- info_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-STOUT=/tmp/stdtest1.txt
-STERR=/tmp/stdtest1.txt
-BACKTITLE="bilgisi"
-dialog --begin 3 3 --clear --backtitle "$BACKTITLE" --title "Kurulum bilgisi STDOUT" \
---textbox /tmp/stdtest1.txt 30 150
-dialog --begin 3 3 --clear --backtitle "$BACKTITLE" --title "Kurulum bilgisi STDERR" \
---textbox /tmp/stdtest2.txt 30 150

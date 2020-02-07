e6e13f0dea21c2da2b51ac7fe2db0e1092e07a10
suphicakir <suphicakir@gmail.com>
2020-02-07 21:47:56 +0300

    info_test.sh dosyasında ki hata düzeltildi.

--- info_test.sh
+++ info_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 STOUT=/tmp/stdtest1.txt
 STERR=/tmp/stdtest1.txt
-BACKTITLE="bilgis"
+BACKTITLE="bilgisi"
 dialog --begin 3 3 --clear --backtitle "$BACKTITLE" --title "Kurulum bilgisi STDOUT" \
 --textbox /tmp/stdtest1.txt 30 150
 dialog --begin 3 3 --clear --backtitle "$BACKTITLE" --title "Kurulum bilgisi STDERR" \
